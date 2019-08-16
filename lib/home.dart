1833814ff00a1a8905062d64f7549307ca4c269e
Jesse Son <json469@aucklanduni.ac.nz>
2019-08-16 16:37:11 +1200

    Add webfeed dependency and simpe feed request list item builder

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:webfeed/webfeed.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -14,21 +15,34 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Cheep'),
-      ),
-      body: FutureBuilder<Response>(
-          future: fetchFeed(),
-          builder: (BuildContext context, AsyncSnapshot<Response> snapshot) {
-            if (snapshot.connectionState == ConnectionState.done)
-              return SingleChildScrollView(
-                  child: Text(snapshot.data.body.toString()));
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[Center(child: CircularProgressIndicator())],
-            );
-          }),
+      appBar: AppBar(title: Text('Cheep')),
+      body: buildFutureBuilder(),
+    );
+  }
+
+  FutureBuilder<Response> buildFutureBuilder() {
+    return FutureBuilder<Response>(
+      future: fetchFeed(),
+      builder: (BuildContext context, AsyncSnapshot<Response> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          final RssFeed feed = RssFeed.parse(snapshot.data.body);
+          return ListView.separated(
+            separatorBuilder: (context, index) => Divider(color: Colors.grey),
+            itemCount: feed.items.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                title: Text(feed.items[index].title),
+              );
+            },
+          );
+        } else {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[Center(child: CircularProgressIndicator())],
+          );
+        }
+      },
     );
   }
 
