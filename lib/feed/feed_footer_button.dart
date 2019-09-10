61c9099d9f291b36c83ba2396d57ece6df592622
Jesse Son <jesse.son@datacom.co.nz>
2019-09-10 22:50:08 +1200

    Remove unused imports, comments, and change button color

--- lib/feed/feed_footer_button.dart
+++ lib/feed/feed_footer_button.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:webfeed/domain/rss_item.dart';
@@ -56,7 +54,7 @@ class FeedFooterButton extends StatelessWidget {
     return Container(
       height: 60.0,
       child: RaisedButton(
-        color: Theme.of(context).primaryColor,
+        color: Theme.of(context).primaryColorDark,
         child: Center(
           child: Text(
             kOpenDealButton,
@@ -76,7 +74,7 @@ class FeedFooterButton extends StatelessWidget {
         Container(
           height: 60.0,
           child: RaisedButton(
-            color: Theme.of(context).primaryColor,
+            color: Theme.of(context).primaryColorDark,
             child: Center(
               child: Text(
                 kOpenDealButton,
@@ -110,14 +108,16 @@ class FeedFooterButton extends StatelessWidget {
 
     return Container(
       height: 60.0,
-      color: _themeData.primaryColorDark,
+      color: _themeData.primaryColor,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
+        reverse: true,
         itemCount: listOfCodes.length,
         itemBuilder: (BuildContext context, int index) {
           return Padding(
-            padding: EdgeInsets.fromLTRB(index != 0 ? 0 : 8.0, 12.0, 8.0, 12.0),
+            padding: EdgeInsets.fromLTRB(8.0, 12.0, index != 0 ? 0 : 8.0, 12.0),
             child: FlatButton(
+              splashColor: Colors.white38,
               color: Colors.transparent,
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(8.0),
@@ -139,32 +139,3 @@ class FeedFooterButton extends StatelessWidget {
     );
   }
 }
-
-// Widget _buildCouponCode(TextTheme textTheme, RssItem item) {
-//   final int nodeID =
-//       int.parse(item.link.substring(item.link.lastIndexOf('/') + 1));
-//   return FutureBuilder<Response>(
-//     future: get(
-//         'https://us-central1-cheep-scraper.cloudfunctions.net/get_coupon_code/?node_id=$nodeID'),
-//     builder: (BuildContext context, AsyncSnapshot<Response> snapshot) {
-//       if (snapshot.connectionState == ConnectionState.done) {
-//         if (snapshot.data.body != '') {
-//           if (snapshot.data.body is String)
-//             return Container(
-//                 margin: const EdgeInsets.only(top: 4.0),
-//                 padding: const EdgeInsets.all(8.0),
-//                 decoration: BoxDecoration(
-//                   color: Theme.of(context).primaryColor,
-//                   borderRadius: BorderRadius.circular(4.0),
-//                 ),
-//                 alignment: Alignment.center,
-//                 child: Text(
-//                   snapshot.data.body,
-//                   style: textTheme.subhead.copyWith(color: Colors.white),
-//                 ));
-//         }
-//       }
-//       return Container();
-//     },
-//   );
-// }
