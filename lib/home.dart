6f060a6709c32c659c40f6a97d612eb6692e8dca
Jesse Son <json469@aucklanduni.ac.nz>
2019-11-27 17:21:36 +1300

    Implement new firestore

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 import 'package:webfeed/webfeed.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:project_cheep/navigations/navigation_drawer.dart';
 import 'package:project_cheep/feed/feed_item.dart';
@@ -10,55 +10,30 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin {
-  Future<Response> _feed;
-  _HomeState() {
-    _feed = _fetchFeed();
-  }
+class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Cheep')),
-      body: _buildFutureBuilder(),
+      body: StreamBuilder(
+          stream: Firestore.instance
+              .collection('rss')
+              .document('cheapies')
+              .snapshots(),
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            if (snapshot.hasData) {
+              return RefreshIndicator(
+                child: _buildFeedListView(
+                    RssFeed.parse(snapshot.data.data['content'])),
+                onRefresh: () {},
+              );
+            } else {
+              return _buildLoadingScreen();
+            }
+          }),
       drawer: NavigationDrawer(),
     );
-  }
-
-  @override
-  bool get wantKeepAlive => true;
-
-  Future<Response> _fetchFeed() async {
-    return get('https://www.cheapies.nz/deals/feed');
-  }
-
-  FutureBuilder<Response> _buildFutureBuilder() {
-    return FutureBuilder<Response>(
-      future: _feed,
-      builder: (BuildContext context, AsyncSnapshot<Response> snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.none:
-            return _buildLoadingScreen();
-          case (ConnectionState.active):
-            return _buildLoadingScreen();
-          case (ConnectionState.waiting):
-            return _buildLoadingScreen();
-          default:
-            return RefreshIndicator(
-              child: _buildFeedListView(RssFeed.parse(snapshot.data.body)),
-              onRefresh: _onRefresh,
-            );
-        }
-      },
-    );
-  }
-
-  Future<void> _onRefresh() {
-    setState(() {
-      _feed = _fetchFeed();
-    });
-    // To prevent "The RefreshIndicator onRefresh callback must return a Future."
-    return Future.delayed(const Duration(milliseconds: 0));
   }
 
   ListView _buildFeedListView(RssFeed feed) {
